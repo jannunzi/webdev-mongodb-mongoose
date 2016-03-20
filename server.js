5f46c333a589dddb2e028c1183f229246d822070
JoseAnnunziatoNeu <jga@ccs.neu.edu>
2016-03-20 18:39:08 -0400

    Created simple express server compatible with openshift.com

---
+++ server.js
@@ -0,0 +1,13 @@
+// install express
+// npm install express --save
+var express    = require('express');
+// create an express instance
+var app        = express();
+
+// use 127.0.0.1:3000 locally
+// use remote IP and PORT if hosted on openshift.com
+var ipaddress  = process.env.OPENSHIFT_NODEJS_IP   || "127.0.0.1";
+var port       = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
+// listen for incoming HTTP requests
+app.listen(port, ipaddress);
