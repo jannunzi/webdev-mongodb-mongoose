786a13b04f47e506cc9eb9c9f3ae7f9d0dce11f6
JoseAnnunziatoNeu <jga@ccs.neu.edu>
2016-03-20 18:41:18 -0400

    Added public directory serving simple index page

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var express    = require('express');
 // create an express instance
 var app        = express();
 
+app.use(express.static(__dirname + '/public'));
+
 // use 127.0.0.1:3000 locally
 // use remote IP and PORT if hosted on openshift.com
 var ipaddress  = process.env.OPENSHIFT_NODEJS_IP   || "127.0.0.1";
